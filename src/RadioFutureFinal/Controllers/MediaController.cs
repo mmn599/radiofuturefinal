2047afa198c9975ed81dfe36828e063f4bdbc042
Matthew Normyle <mmnormyle@gmail.com>
2017-02-25 17:55:44 -0600

    basic DAL working.

--- src/RadioFutureFinal/Controllers/MediaController.cs
+++ src/RadioFutureFinal/Controllers/MediaController.cs
@@ -7,145 +7,22 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RadioFutureFinal.Data;
 using RadioFutureFinal.Models;
+using RadioFutureFinal.DAL;
 
 namespace RadioFutureFinal.Controllers
 {
     public class MediaController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly IDbRepository _db;
 
-        public MediaController(ApplicationDbContext context)
+        public MediaController(IDbRepository db)
         {
-            _context = context;    
+            _db = db;    
         }
 
-        // GET: Media
-        public async Task<IActionResult> Index()
+        public string Index()
         {
-            return View(await _context.Media.ToListAsync());
-        }
-
-        // GET: Media/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var media = await _context.Media.SingleOrDefaultAsync(m => m.MediaID == id);
-            if (media == null)
-            {
-                return NotFound();
-            }
-
-            return View(media);
-        }
-
-        // GET: Media/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Media/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("MediaID,Dislikes,Likes,ThumbURL,UserID,UserName,YTVideoID")] Media media)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(media);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            return View(media);
-        }
-
-        // GET: Media/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var media = await _context.Media.SingleOrDefaultAsync(m => m.MediaID == id);
-            if (media == null)
-            {
-                return NotFound();
-            }
-            return View(media);
-        }
-
-        // POST: Media/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("MediaID,Dislikes,Likes,ThumbURL,UserID,UserName,YTVideoID")] Media media)
-        {
-            if (id != media.MediaID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(media);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!MediaExists(media.MediaID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction("Index");
-            }
-            return View(media);
-        }
-
-        // GET: Media/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var media = await _context.Media.SingleOrDefaultAsync(m => m.MediaID == id);
-            if (media == null)
-            {
-                return NotFound();
-            }
-
-            return View(media);
-        }
-
-        // POST: Media/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var media = await _context.Media.SingleOrDefaultAsync(m => m.MediaID == id);
-            _context.Media.Remove(media);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
-
-        private bool MediaExists(int id)
-        {
-            return _context.Media.Any(e => e.MediaID == id);
+            return _db.GetAllMedia().ToString();
         }
     }
 }
--- src/RadioFutureFinal/Controllers/UsersController.cs
+++ src/RadioFutureFinal/Controllers/UsersController.cs
@@ -7,145 +7,22 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RadioFutureFinal.Data;
 using RadioFutureFinal.Models;
+using RadioFutureFinal.DAL;
 
 namespace RadioFutureFinal.Controllers
 {
     public class UsersController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly IDbRepository _db;
 
-        public UsersController(ApplicationDbContext context)
+        public UsersController(IDbRepository db)
         {
-            _context = context;    
+            _db = db;    
         }
 
-        // GET: Users
-        public async Task<IActionResult> Index()
+        public string Index()
         {
-            return View(await _context.User.ToListAsync());
-        }
-
-        // GET: Users/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.User.SingleOrDefaultAsync(m => m.UserID == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return View(user);
-        }
-
-        // GET: Users/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Users/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("UserID,Name,QueuePosition,VideoTime,Waiting,YTPlayerState")] User user)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(user);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            return View(user);
-        }
-
-        // GET: Users/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.User.SingleOrDefaultAsync(m => m.UserID == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            return View(user);
-        }
-
-        // POST: Users/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("UserID,Name,QueuePosition,VideoTime,Waiting,YTPlayerState")] User user)
-        {
-            if (id != user.UserID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(user);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!UserExists(user.UserID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction("Index");
-            }
-            return View(user);
-        }
-
-        // GET: Users/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.User.SingleOrDefaultAsync(m => m.UserID == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return View(user);
-        }
-
-        // POST: Users/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var user = await _context.User.SingleOrDefaultAsync(m => m.UserID == id);
-            _context.User.Remove(user);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
-
-        private bool UserExists(int id)
-        {
-            return _context.User.Any(e => e.UserID == id);
+            return _db.GetAllUsers().ToString();
         }
     }
 }
