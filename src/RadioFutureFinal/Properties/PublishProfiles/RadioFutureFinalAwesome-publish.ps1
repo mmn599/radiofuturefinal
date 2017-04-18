5ad02abec800515ac567c2e3e571395b8b8428f0
Matthew Normyle <mmnormyle@gmail.com>
2017-04-17 19:38:16 -0500

    publish profiles

---
+++ src/RadioFutureFinal/Properties/PublishProfiles/RadioFutureFinalAwesome-publish.ps1
@@ -0,0 +1,19 @@
+﻿[cmdletbinding(SupportsShouldProcess=$true)]
+param($publishProperties=@{}, $packOutput, $pubProfilePath)
+
+# to learn more about this file visit https://go.microsoft.com/fwlink/?LinkId=524327
+
+try{
+    if ($publishProperties['ProjectGuid'] -eq $null){
+        $publishProperties['ProjectGuid'] = '939c9613-c0c7-45f2-9456-a15bc6a0206d'
+    }
+
+    $publishModulePath = Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'publish-module.psm1'
+    Import-Module $publishModulePath -DisableNameChecking -Force
+
+    # call Publish-AspNet to perform the publish operation
+    Publish-AspNet -publishProperties $publishProperties -packOutput $packOutput -pubProfilePath $pubProfilePath
+}
+catch{
+    "An error occurred during publish.`n{0}" -f $_.Exception.Message | Write-Error
+}
