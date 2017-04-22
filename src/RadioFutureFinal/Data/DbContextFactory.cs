d77cc8d5b2a7060e872b7b1583d282c82f566a0f
Matthew Normyle <mmnormyle@gmail.com>
2017-04-22 09:26:46 -0500

    cleaner dbcontextfactory

--- src/RadioFutureFinal/Data/DbContextFactory.cs
+++ src/RadioFutureFinal/Data/DbContextFactory.cs
@@ -11,28 +11,14 @@ namespace RadioFutureFinal.Data
 {
     public class DbContextFactory : IDbContextFactory<ApplicationDbContext>
     {
-        IConfigurationRoot _configuration;
-        
-        // Only used for EF framework
-        public DbContextFactory()
-        {
-            var directory = System.IO.Directory.GetCurrentDirectory() + "../../../..";
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(directory)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-            builder.AddEnvironmentVariables();
-            _configuration = builder.Build();
-        }
-
-        public DbContextFactory(IConfigurationRoot configuration)
-        {
-            _configuration = configuration;
-        }
-
         public ApplicationDbContext Create(DbContextFactoryOptions options)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(options.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            var configuration = builder.Build();
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
             return new ApplicationDbContext(optionsBuilder.Options);
         }
     }
