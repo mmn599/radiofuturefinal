4baea1f81abc44d2496a7f464f21bbe824a923b8
Matthew Normyle <mmnormyle@gmail.com>
2017-04-26 18:44:23 -0500

    Adding tests

---
+++ RadioFutureFinal.Tests/ControllerTests/HomeControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RadioFutureFinal.Controllers;
+using Xunit;
+
+namespace RadioFutureFinal.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexReturnsCorrectView()
+        {
+            var homeController = new HomeController();
+            var viewResults = (ViewResult)homeController.Index();
+            var viewName = viewResults.ViewName;
+            Assert.True(viewName == "Index");
+        }
+
+    }
+}
---
+++ RadioFutureFinal.Tests/WebSocketTests/WebSocketSenderTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RadioFutureFinal.Tests.WebSocketTests
+{
+    public class WebSocketSenderTests
+    {
+    }
+}
