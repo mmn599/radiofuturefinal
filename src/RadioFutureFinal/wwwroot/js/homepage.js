a4aa0e774c087d28bb469fc7844674a7126352ac
Matthew Normyle <mmnormyle@gmail.com>
2017-05-10 14:52:03 -0500

    homepage ui

--- src/RadioFutureFinal/wwwroot/js/homepage.js
+++ src/RadioFutureFinal/wwwroot/js/homepage.js
@@ -1,12 +1,18 @@
 $(document).ready(function(){
 
 	$("#txt_group_join").keypress(function(e) {
-		if(e.which===13) {
-			$(".wrapper").fadeOut(700, function() {
-				window.location.href = "/rooms/" + $("#txt_group_join").val();
-			});
+        if (e.which === 13) {
+            goToPlaylist();
 		}
 	});
+
+    $("#btn_go").click(goToPlaylist);
+
+    function goToPlaylist() {
+        $(".wrapper").fadeOut(700, function() {
+            window.location.href = "/rooms/" + $("#txt_group_join").val();
+        });
+    }
 
 });
 
